78a54e2353f3f0c25bb4bb3e3293abd0e7ad9077
dhruv patel <dhruvjpatel5@gmail.com>
2025-06-28 15:33:37 +0530

    create dockerimage in nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const PORT = process.env.PORT ? +process.env.PORT:8000;
+const app = express();
+
+app.get("/",(req,res)=>{
+    return res.json({
+        status:"Success",
+        message:"Hello from express",
+    })
+})
+
+app.listen(PORT,()=> console.log(`Server start on PORT ${PORT}`));
